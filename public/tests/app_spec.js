02f213f6c987074ade866c620f8f993adb10f103
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2017-10-31 21:31:14 +0900

    2200 add add test

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,6 +1,10 @@
 describe('LearnJS', function() {
-    it('canshow a problem view', function() {
+    it('can show a problem view', function() {
         learnjs.showView('#problem-1');
         expect($('.view-container .problem-view').length).toEqual(1);
+    });
+    it('shows the landing page view when there is no hash.', function() {
+        learnjs.showView();
+        expect($('.view-container .landing-view').length).toEqua(1);
     });
 });
