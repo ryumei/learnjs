a50f55296a6bca6caf86c29cd9bdb13509bd541e
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2017-11-02 00:10:22 +0900

    3200 pass the tests

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 'use strict';
 var learnjs = {};
 
-leanjs.problems = [
+//
+learnjs.problems = [
     {
         description: "What is truth?",
         code: "function problem() { return __; }"
@@ -11,10 +12,13 @@ leanjs.problems = [
         code: "function problem() { return 42 === 6 * __; }"
     }    
 ];
+//
 
-learnjs.problemView = function(problemNumber) {
+learnjs.problemView = function(data) {
+    var problemNumber = parseInt(data, 10);
     var view = $('.templates .problem-view').clone();
-    view.find('.title').text('Problem #' + problemNumber + ' Coming soon!');
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(learnjs.problems[problemNumber], view);
     return view;
 }
 
@@ -34,4 +38,10 @@ learnjs.appOnReady = function() {
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
+}
+
+learnjs.applyObject = function(obj, elem) {
+    for (var key in obj) {
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
 }
