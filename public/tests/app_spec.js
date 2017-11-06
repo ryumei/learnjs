529f4dc83f5b6db2b2792091d7b28bcae1dd8072
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2017-11-06 22:06:11 +0900

    local changes

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -32,11 +32,13 @@ describe('LearnJS', function() {
 
     describe('answer section', function() {
         it('can check a correct answer by hitting a button', function() {
+            //var view = learnjs.problemView("1");
             view.find('.answer').val('true');
             view.find('.check-btn').click();
             expect(view.find('.result').text()).toEqual('Correct!');
         });
         it('rejects an incorrect answer', function() {
+            //var view = learnjs.problemView("1");
             view.find('.answer').val('false');
             view.find('.check-btn').click();
             expect(view.find('.result').text()).toEqual('Incorrect!');
