5eec71a6bf2ac0daa40b8f9b635dede5713929db
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2017-11-08 23:55:10 +0900

    4301

--- public/app.js
+++ public/app.js
@@ -39,6 +39,12 @@ learnjs.applyObject = function(obj, elem) {
   }
 };
 
+learnjs.addProfileLink = function(profile) {
+  var link = learnjs.template('profile-link');
+  link.find('a').text(profile.email);
+  $('.signin-bar').prepend(link);
+}
+
 learnjs.flashElement = function(elem, content) {
   elem.fadeOut('fast', function() {
     elem.html(content);
@@ -99,9 +105,18 @@ learnjs.landingView = function() {
   return learnjs.template('landing-view');
 }
 
+learnjs.profileView = function() {
+  var view = learnjs.template('profile-view');
+  learnjs.identity.done(function(identity) {
+    view.find('.email').text(identity.email);
+  });
+  return view;
+}
+
 learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView,
+    '#profile': learnjs.profileView,
     '#': learnjs.landingView,
     '': learnjs.landingView
   };
@@ -118,19 +133,20 @@ learnjs.appOnReady = function() {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
+  learnjs.identity.done(learnjs.addProfileLink);
 }
 
 learnjs.landingView = function() {
   return learnjs.template('landing-view');
 }
 
-learnjs.awsRegresh = function() {
+learnjs.awsRefresh = function() {
   var deffered = new $.Deferred();
   AWS.config.credentials.refresh(function(err) {
     if (err) {
-      deferred.reject(err);
+      deffered.reject(err);
     } else {
-      deferred.resolve(AWS.config.credentials.identityId);
+      deffered.resolve(AWS.config.credentials.identityId);
     }
   });
   return deffered.promise();
